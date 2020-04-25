f5c3ac907ba6e9022655893f1d903df2bdb2051a
SolomonLake <lakegh@gmail.com>
2020-04-25 18:43:45 +0000

    Deploying to gh-pages from  @ 6a63aa19592ae9ccec0537d678a8d02f1c4512dd 🚀

---
+++ precache-manifest.e59ae1131a53e4b231c6376ee07bc77a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23be2b1a067a4d0b517465fc0269c06",
+    "url": "/project-tiles/index.html"
+  },
+  {
+    "revision": "37d06c3b0b04e0935a07",
+    "url": "/project-tiles/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "10c5656fc0655651669e",
+    "url": "/project-tiles/static/js/2.b5d99765.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/project-tiles/static/js/2.b5d99765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d06c3b0b04e0935a07",
+    "url": "/project-tiles/static/js/main.0116eb98.chunk.js"
+  },
+  {
+    "revision": "2f1f4ff2c24fee03f40f",
+    "url": "/project-tiles/static/js/runtime-main.c69db907.js"
+  },
+  {
+    "revision": "e7dba3188b6d123029d0ad0973d6b52d",
+    "url": "/project-tiles/static/media/PrehistoricIcon_44_b.e7dba318.png"
+  },
+  {
+    "revision": "f386ff8ad9fcd6331e26d4c549b212ec",
+    "url": "/project-tiles/static/media/SGI_86.f386ff8a.png"
+  },
+  {
+    "revision": "0f20a41da3948399e756e7a7358216b0",
+    "url": "/project-tiles/static/media/gems_add_152_b.0f20a41d.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project-tiles/precache-manifest.74f416fb9f7c812d9db4bd81ae4fd215.js"
+  "/project-tiles/precache-manifest.e59ae1131a53e4b231c6376ee07bc77a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-tiles/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
